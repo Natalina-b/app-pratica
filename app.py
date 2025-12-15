ef5437b207fa4d844e84e3df3854de2188d49b83
Natalina-b <natalinaborgesunicv@gmail.com>
2025-12-15 11:04:35 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import streamlit as st
+from steamlit_option_menu import option_menu
+import numpy asnp
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
 st.title("Introduzindo os elimentes de streamlit")
+menu= option_menu(menu_title="Menu",
+                 options=["Inicio", "Gráfico", "Estatísticas", "Gráficos dinâmicos", "Widgets","Formulário"],
+                 icons=["bar-chart", "bar-chart-line", "toogles", "bar-chart"],
+                 menu_icon="cast",
+                 default_index=0
+                 orientation="horinzantal"
+
+                 )
+with st.sidebar:
+  st.sucess("**UPLOAD DE DADOS")
